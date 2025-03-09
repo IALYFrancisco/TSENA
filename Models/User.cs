0387cf4ca25548be96775e0753aad332ae9bcbc1
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-09 19:56:38 +0300

    définition des propriétés du model des données des utilisateurs

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TSENA.Models;
+
+public class User {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    private string? Password { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime signInDate { get; set; }
+}
