28a8bca7b8b7f2289ffc68f59063064636994719
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-10 15:13:55 +0300

    création de fichier de migration pour la modification du Modèle User

---
+++ Migrations/User/20250310121251_ModificationDuPropPassword.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TSENA.Migrations.User
+{
+    /// <inheritdoc />
+    public partial class ModificationDuPropPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "User");
+        }
+    }
+}
