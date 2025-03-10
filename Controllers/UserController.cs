37cf9658e5b72c4a24101ec6acfffba2900e9353
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-10 14:57:02 +0300

    Définition de l'action d'inscription d'utilisateurs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -14,5 +16,34 @@ namespace TSENA.Controllers {
         public UserController(UserContext context){
             _context = context;
         }
+
+       [HttpPost]
+public async Task<IActionResult> Register(User model) {
+    if(ModelState.IsValid){
+        var existingUser = await _context.User.FirstOrDefaultAsync(u => u.Email == model.Email);
+        if(existingUser != null){
+            ViewData["Error"] = "Cet email existe déjà";
+            return View(model);
+        }
+
+        // Correction ici
+        model.Password = HashPassword(model.Password);
+
+        _context.User.Add(model);
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Login");
+    }
+
+    return View(model);
+}
+
+        private string HashPassword(string Password){
+            using (var sha256 = SHA256.Create()){
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(Password));
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
     }
 }
