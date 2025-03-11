b1eb874373208a5da6aea143d8740c04427948d9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-11 10:48:59 +0300

    changement de modèle User

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@ namespace TSENA.Models;
 public class User {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public string? Email { get; set; }
+    public required string Email { get; set; }
     public required string Password { get; set; }
     [DataType(DataType.Date)]
     public DateTime SignInDate { get; set; }
