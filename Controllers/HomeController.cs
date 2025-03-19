742fa8b01f6e99fca8bca9c22e255ad3c8952707
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-19 11:04:47 +0300

    modification de certaines actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,22 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TSENA.Models;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 
 namespace TSENA.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -18,9 +24,22 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Parametre()
+    public async Task<IActionResult> Parametre()
     {
-        return View();
+        // Récupère l'email de l'utilisateur connecté à partir des Claims
+        var email = User.FindFirstValue(ClaimTypes.Email);
+
+         // Cherche l'utilisateur dans la base de données avec l'email récupéré
+        var user = await _context.User.FirstOrDefaultAsync(u => u.Email == email);
+
+        // Crée un modèle pour transmettre les informations (nom et email)
+        var userConnected = new User {
+            Name = user?.Name,
+            Email = user?.Email
+        };
+
+        // Passe le modèle à la vue
+        return View(userConnected);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
