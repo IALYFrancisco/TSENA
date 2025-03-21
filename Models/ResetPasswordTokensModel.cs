727494724d1ccbbc0692f7b00e932e73d74fc4ff
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-03-21 19:57:22 +0300

    Ajout du modèle de donnée pour la fonctionnalié de mot de passe oublié

--- Models/ResetPasswordTokensModel.cs
+++ Models/ResetPasswordTokensModel.cs
@@ -4,19 +4,17 @@ using System.ComponentModel.DataAnnotations;
 namespace TSENA.Models;
 public class ResetPasswordTokens
 {
-    [Key]
     public int Id {get; set;}
 
-    [Required]
-    [EmailAddress]
-    [MaxLength(256)]
     public string? Email {get; set;}
 
-    [Required]
-    [MaxLength(256)]
     public string? Token {get; set;}
 
     public DateTime ExpirationDate {get;set;}
 
     public bool IsUsed {get;set;} = false;
+
+    public string? NewPassword { get; set; }
+
+    public string? ConfirmPassword { get; set; }
 }
