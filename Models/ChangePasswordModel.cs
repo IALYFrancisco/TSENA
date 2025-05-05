113646839c9419c18f7fa896bd10b5974cf95515
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-05 14:17:06 +0300

    changement du modèle de données

--- Models/ChangePasswordModel.cs
+++ Models/ChangePasswordModel.cs
@@ -3,20 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace TSENA.Models;
 
 public class ChangePassword {
-    [Required]
-    [DataType(DataType.Password)]
-    [Display(Name="Ancien mot de passe")]
     public string? CurrentPassword { get; set; }
-
-    [Required]
-    [StringLength(100, MinimumLength=6)]
-    [DataType(DataType.Password)]
-    [Display(Name="Nouveau mot de passe")]
     public string? NewPassword { get; set; }
-
-    [Required]
-    [Compare("NewPassword")]
-    [DataType(DataType.Password)]
-    [Display(Name="Confirmer le nouveau mot de passe")]
     public string? ConfirmPassword { get; set; }
 }
